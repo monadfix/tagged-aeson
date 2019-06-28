bb9c7601d40c5e0a0bf78f35deae34a9a9479108
Artyom Kazak <yom@artyom.me>
2019-06-28 14:21:54 +0300

    Vector tests

--- test/TaggedSpec.hs
+++ test/TaggedSpec.hs
@@ -19,13 +19,15 @@ import Data.Aeson.Tagged
 import qualified Data.Text as T
 import Data.Text (Text)
 import qualified Data.List.NonEmpty as NE
+import qualified Data.Vector as V
+import Data.Vector (Vector)
 -- import qualified Data.Aeson          as A
 -- import qualified Data.Aeson.Types    as A
 -- import qualified Data.Aeson.TH       as A
 -- import qualified Data.Aeson.Internal as A
 -- import qualified Data.Aeson.Encoding as E
 import Data.Aeson.Types (Result(..))
-import qualified GHC.TypeLits
+import qualified GHC.TypeLits as TypeLits
 
 import Test.Hspec
 import Util
@@ -34,6 +36,7 @@ spec :: Spec
 spec = do
     liftingListSpec
     liftingNonEmptySpec
+    liftingVectorSpec
 
 -- Do default definitions for FromJSON and ToJSON methods work?
 -- Do "parseJSON @Aeson" and "toJSON @Aeson" work?
@@ -43,7 +46,6 @@ spec = do
 -- Does "deriving via WithAeson" work?
 -- Does "deriving via WithAeson1" work? On stuff that has FromJSON1? On Set and HashSet?
 -- Does encoding and decoding Rational work?
--- parseVector, vectorToJSON, vectorToEncoding
 -- parseSet, setToJSON, setToEncoding
 -- parseHashSet, hashSetToJSON, hashSetToEncoding
 
@@ -98,6 +100,29 @@ liftingNonEmptySpec = do
                 `shouldBe` [encoding|["modded:a"]|]
 
 ----------------------------------------------------------------------------
+-- parseVector, vectorToJSON, vectorToEncoding
+----------------------------------------------------------------------------
+
+liftingVectorSpec :: Spec
+liftingVectorSpec = do
+    describe "parseVector" $ do
+        it "uses tagged-aeson for vector elements" $ do
+            parse (parseVector @Modded @Text) [value|["a"]|]
+                `shouldBe` Error "expected modded text"
+            parse (parseVector @Modded @Text) [value|["modded:a", "modded:b"]|]
+                `shouldBe` Success (V.fromList ["a", "b"])
+
+    describe "vectorToJSON" $ do
+        it "uses tagged-aeson for vector elements" $ do
+            vectorToJSON @Modded @Text (V.fromList ["a"])
+                `shouldBe` [value|["modded:a"]|]
+
+    describe "vectorToEncoding" $ do
+        it "uses tagged-aeson for vector elements" $ do
+            vectorToEncoding @Modded @Text (V.fromList ["a"])
+                `shouldBe` [encoding|["modded:a"]|]
+
+----------------------------------------------------------------------------
 -- Helpers
 ----------------------------------------------------------------------------
 
@@ -109,25 +134,29 @@ instance FromJSON Modded Text where
             Nothing -> fail "expected modded text"
             Just s' -> pure s'
 
-instance GHC.TypeLits.TypeError
-             ('GHC.TypeLits.Text "[]@Modded should never be used")
+instance TypeLits.TypeError ('TypeLits.Text "[]@Modded should never be used")
       => FromJSON Modded [a] where
     parseJSON = undefined
 
-instance GHC.TypeLits.TypeError
-             ('GHC.TypeLits.Text "NonEmpty@Modded should never be used")
+instance TypeLits.TypeError ('TypeLits.Text "NonEmpty@Modded should never be used")
       => FromJSON Modded (NonEmpty a) where
+    parseJSON = undefined
+
+instance TypeLits.TypeError ('TypeLits.Text "Vector@Modded should never be used")
+      => FromJSON Modded (Vector a) where
     parseJSON = undefined
 
 instance ToJSON Modded Text where
     toJSON s = using @Aeson (toJSON ("modded:" <> s))
 
-instance GHC.TypeLits.TypeError
-             ('GHC.TypeLits.Text "[]@Modded should never be used")
+instance TypeLits.TypeError ('TypeLits.Text "[]@Modded should never be used")
       => ToJSON Modded [a] where
     toJSON = undefined
 
-instance GHC.TypeLits.TypeError
-             ('GHC.TypeLits.Text "NonEmpty@Modded should never be used")
+instance TypeLits.TypeError ('TypeLits.Text "NonEmpty@Modded should never be used")
       => ToJSON Modded (NonEmpty a) where
+    toJSON = undefined
+
+instance TypeLits.TypeError ('TypeLits.Text "Vector@Modded should never be used")
+      => ToJSON Modded (Vector a) where
     toJSON = undefined
